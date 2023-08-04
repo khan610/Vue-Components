a3357274af682b111b2ac30bce8ff5fa65db0de7
Andrea Trajkovic <trajkovicandrea@gmail.com>
2023-08-04 22:20:25 +0200

    Update

--- app.js
+++ app.js
@@ -4,18 +4,38 @@ const app = Vue.createApp({
       detailsAreVisible: false,
       friends: [
         {
-          id: 'manuel',
-          name: 'Manuel Lorenz',
-          phone: '030493 30403 4039',
-          email: 'manuel@sdsdgmail.com',
-        },
-        {
           id: 'julie',
           name: 'Julie Khan',
           phone: '9334 39493 394',
           email: 'julie@khgangmail.com',
         },
       ],
+    };
+  },
+});
+
+app.component('friend-contact', {
+  template: `
+  <li>
+          <h2>{{ friend.name}}</h2>
+          <button @click="toggleDetails">
+            {{ detailsAreVisible ? 'Hide' : 'Show'}} Details
+          </button>
+          <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+          </ul>
+        </li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'manuel',
+        name: 'Manuel Lorenz',
+        phone: '030493 30403 4039',
+        email: 'manuel@sdsdgmail.com',
+      },
     };
   },
   methods: {
